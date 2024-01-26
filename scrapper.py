04679086caab24ad694e96f3a04b159a33736d25
Mayonnaise-Slap <368758@niuitmo.ru>
2024-01-26 12:17:06 +0300

    added link scrapping for possible interest

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
+import time
 
 
 def get_soup(n_page: int) -> BeautifulSoup:
@@ -12,27 +13,42 @@ def get_soup(n_page: int) -> BeautifulSoup:
     return BeautifulSoup(r.text, "html.parser")
 
 
-def get_headings(soup: BeautifulSoup) -> list[list]:
+def get_contents(soup: BeautifulSoup) -> list[list]:
     headings = []
     for article in soup.find_all("article"):
-        score_index = 2
-
-        heading = article.find_all("span")[2].text
-        attrs = article.find_all("title")
-        if len(attrs) == 6:
-            score_index += 1
-        rez = re.findall(r"[-+]?(?:\d*\.*\d+)", attrs[score_index].text)[1:]
-        rez.insert(0, heading)
-        headings.append(rez)
+        info = get_heading(article)
+        info.append(f"https://habr.com{article.find_all("a")[2]["href"]}")
+        headings.append(info)
     return headings
 
 
+def get_heading(article: BeautifulSoup) -> list[str]:
+    score_index = 2
+
+    heading = article.find_all("span")[2].text
+    attrs = article.find_all("title")
+    if len(attrs) == 6:
+        score_index += 1
+    rez = re.findall(r"[-+]?(?:\d*\.*\d+)", attrs[score_index].text)[1:]
+    rez.insert(0, heading)
+    return rez
+
+
 def dump_heading(headings: list[list]) -> None:
-    df = pd.DataFrame(headings, columns=["Heading", "up", "down"])
+    df = pd.DataFrame(headings, columns=["Heading", "up", "down", "link"])
     df["date"] = pd.Timestamp.today().date()
     df.to_csv("headings.csv", mode="a", index=False, header=False)
 
 
-if __name__ == "__main__":
+def main():
     for i in range(4):
-        dump_heading(get_headings(get_soup(i)))
+        dump_heading(get_contents(get_soup(i+1)))
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
+
+# while True:
+#     main()
+#     time.sleep(86400)
